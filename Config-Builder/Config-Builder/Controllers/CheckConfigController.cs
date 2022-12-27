a7135da2fc890b43aa16be8f80347563530fee7e
Sourov Halder <sourov.eps@gmail.com>
2022-12-27 15:10:37 +0600

    Added controller to check results.

---
+++ Config-Builder/Config-Builder/Controllers/CheckConfigController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Config_Builder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CheckConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("JsonFromApp")]
+
+        public IActionResult DataFromApp()
+        {
+            var appConfig = Services.AppConfig.GetAppConfig.JsonFromApp();
+
+            return Ok(appConfig);
+        }
+
+        [HttpGet("JsonFromDatabase")]
+
+        public IActionResult DataFromDatabase()
+        {
+            var appConfig = Services.AppConfig.GetAppConfig.JsonFromDatabase();
+
+            return Ok(appConfig);
+        }
+
+        [HttpGet("MergedJson")]
+
+        public IActionResult MergedData()
+        {
+            var appConfig = Services.AppConfig.GetAppConfig.JsonMerger();
+
+            return Ok(appConfig);
+        }
+
+        [HttpGet("CheckCustomConfig")]
+
+        public IActionResult CustomConfig()
+        {
+            return Ok(_configuration["SecretKey"]);
+        }
+    }
+}
